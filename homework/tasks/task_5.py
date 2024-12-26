ca13bda1d18e3aa5e8d43531e4d9ee5886b69cb2
moxeeeem <moxeeeem@gmail.com>
2024-12-26 23:36:43 +0300

    add homework (tasks 1-6)

--- homework/tasks/task_5.py
+++ homework/tasks/task_5.py
@@ -2,18 +2,26 @@ import asyncio
 from typing import Coroutine
 
 
-async def limit_execution_time(coro: Coroutine, max_execution_time: float) -> None:
-    # Функция принимает на вход корутину, которую необходимо запустить, однако иногда она выполняется
-    # слишком долго, это время необходимо ограничить переданным на вход количеством секунд.
+async def limit_execution_time(coro: Coroutine,
+                               max_execution_time: float) -> None:
+    # Функция принимает на вход корутину, которую необходимо запустить,
+    # однако иногда она выполняется слишком долго, это время необходимо
+    # ограничить переданным на вход количеством секунд.
     #
-    # Тест проверяет, что каждая переданная корутина была запущена, и все они завершились за заданное
-    # время.
-    #
-    # YOUR CODE GOES HERE
+    # Тест проверяет, что каждая переданная корутина была запущена, и
+    # все они завершились за заданное время.
+    try:
+        await asyncio.wait_for(coro, max_execution_time)
+    except asyncio.TimeoutError:
+        pass
 
 
-async def limit_execution_time_many(*coros: Coroutine, max_execution_time: float) -> None:
-    # Функция эквивалентна limit_execution_time, но корутин на вход приходит несколько.
-    #
-    # YOUR CODE GOES HERE
-
+async def limit_execution_time_many(*coros: Coroutine,
+                                    max_execution_time: float) -> None:
+    # Функция эквивалентна limit_execution_time, но корутин на вход
+    # приходит несколько.
+    tasks = [asyncio.create_task(coro) for coro in coros]
+    try:
+        await asyncio.wait_for(asyncio.gather(*tasks), max_execution_time)
+    except asyncio.TimeoutError:
+        pass
